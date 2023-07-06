7814d67dd7b9a6980c070b8a821d3e4abe0f1a8f
Hussseinkizz <hssnkizz@gmail.com>
2023-07-06 17:44:29 +0300

    session: operators

---
+++ operators.js
@@ -0,0 +1,32 @@
+// * Equals, = vs == vs ===
+// = assignment operator
+// == equality operator, checks value only
+// === strict equality operator, checks value and type
+
+// let a = 1;
+// let b = '1';
+
+// console.log(a === b);
+
+// * Logical And &&
+// let x = 1;
+// if (true && false) {
+//   console.log('Hey!');
+// }
+
+// * post vs pre incrememt and decrement operators
+// let m = 1++;
+// let n = ++1;
+
+// * coercion
+// console.log(1 + '1'); // 11
+// console.log(2 / '2'); // 22
+// console.log(2 / 'hi'); // 2hi
+// console.log(2 * '1'); // 21
+// console.log(2 - '1'); // err
+
+// * type casting
+// let a = '1';
+// console.log(typeof a);
+// let b = Number(a);
+// console.log(typeof b, b);
